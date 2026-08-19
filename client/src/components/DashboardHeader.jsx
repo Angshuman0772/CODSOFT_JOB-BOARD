fb60706032b6dbe8c6be0dce2a2fe5ce94690c6f
Angshuman <angshuman1018@gmail.com>
2026-08-19 11:15:34 +0530

    feat(DashboardHeader): integrate company data display in header; refactor profile section

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -1,27 +1,31 @@
-import {Link} from "react-router-dom";
-import { assets } from "../assets/assets";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { Link } from "react-router-dom";
 import { BriefcaseBusiness } from "lucide-react";
 import "./DashboardHeader.css";
 
 const DashboardHeader = () => {
+  const { companyData } = useContext(AppContext);
+
   return (
     <header className="dashboard-header">
       <Link to="/" className="logo">
         <BriefcaseBusiness size={24} />
         <span>HireFlow</span>
       </Link>
-      <div className="header-profile">
-        <div className="header-text">
-          <h3>Slack</h3>
-          <p>Richard Smith • Recruiter</p>
-        </div>
+      {companyData && (
+        <div className="header-profile">
+          <div className="header-text">
+            <h3>{companyData.name}</h3>
+          </div>
 
-        <img
-          src={assets.company_icon}
-          alt="Spotify Logo"
-          className="company-logo"
-        />
-      </div>
+          <img
+            src={companyData.image}
+            alt="Spotify Logo"
+            className="company-logo"
+          />
+        </div>
+      )}
     </header>
   );
 };
