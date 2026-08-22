d4c7726485b4ae7de7a7a15718c9ab0a64093fb1
Angshuman <angshuman1018@gmail.com>
2026-08-22 12:55:16 +0530

    disable telemetry clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,11 @@ import App from "./App.jsx";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    afterSignOutUrl="/"
+    telemetry={false}
+  >
     <BrowserRouter>
       <AppContextProvider>
         <App />
